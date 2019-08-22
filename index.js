fd70ee7e8522833c367f77d0a6b3b90c36ee3691
KauanR <50336463+KauanR@users.noreply.github.com>
2019-08-22 17:12:34 -0300

    esqueleto pronto

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+
+// SQL
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host     : 'localhost',
     port     : 3306,
     user     : 'root',
-    password : '123456789',
+    password : 'password',
     database : 'mydb'
 });
-
-
-app.listen(3000, function() {
-    console.log('Rodando na porta 3000');
-});
-
 connection.connect(function(err){
     if(err) {
         return console.log(err);
     }
     console.log('conectou!');
-})
+});
+
+// BODY PARSER - receber JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTER
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+
+
+app.listen(3000, function() {
+    console.log('Rodando na porta 3000');
+});
