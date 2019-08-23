53c35a8344c83720c975c5fd943b1232fe891df8
Igor Almeida <inazumaseleven04@gmail.com>
2019-08-23 20:47:27 -0300

    implmentando null checkers melhores

--- src/app/controllers/Controller.js
+++ src/app/controllers/Controller.js
@@ -13,17 +13,46 @@ module.exports = class Controller {
         this.nomePlural = nomePlural
         this.DAO = new DAO(tabela)
 
-        this.validationSchema = {
+        this.attrs = []
+
+        this.attrsQuery = []
+
+        this.attrNotNull = []
+
+        this.validationSchema = {}
+
+        Object.assign(this.validationSchema, this.gerarValidationSchema(validationSchema))
+
+        if (!naoGerarTodasRotas) {
+            this.gerarRotaBusca()
+            this.gerarRotaDeleta()
+            this.gerarRotaAtualiza()
+            this.gerarRotaAdicionaUm()
+        }
+    }
+
+    gerarValidationSchema(validationSchema) {
+        const copy = JSON.parse(JSON.stringify(validationSchema))
+
+        const keys = Object.keys(copy)
+
+        for (let i = 0; i < keys.length; i++) {
+            const k = keys[i]
+
+            copy[k].in = ["query", "body"]
+
+            if (copy[k].notNull) {
+                this.attrNotNull.push(k)
+                delete copy[k].notNull
+            }
+        }
+
+        Object.assign(copy, {
             id: {
                 in: ["query"],
                 isInt: {
                     options: {
                         min: 1
-                    }
-                },
-                optional: {
-                    options: {
-                        nullable: true
                     }
                 },
                 errorMessage: "O valor deve ser inteiro maior que 0."
@@ -35,76 +64,41 @@ module.exports = class Controller {
                         min: 1
                     }
                 },
-                optional: {
-                    options: {
-                        nullable: true
-                    }
-                },
                 errorMessage: "O valor deve ser inteiro maior que 0."
             },
             ordem: {
                 in: ["query"],
                 isIn: ["ASC", "DESC"],
-                optional: {
-                    options: {
-                        nullable: true
-                    }
-                },
                 errorMessage: "O valor deve ser ASC ou DESC."
             },
             ordenarPor: {
                 in: ["query"],
-                isIn: ["id"].concat(Object.keys(validationSchema)),
-                optional: {
-                    options: {
-                        nullable: true
-                    }
-                },
+                isIn: ["id"].concat(keys),
                 errorMessage: "O valor deve ser um atributo válido."
-            }
-        }
-
-        this.validationSchemaWithoutNullChecker = {}
-
-        Object.assign(this.validationSchemaWithoutNullChecker, this.validationSchema)
-
-        Object.assign(this.validationSchemaWithoutNullChecker, this.gerarValidationSchema(validationSchema, true))
-
-        Object.assign(this.validationSchema, this.gerarValidationSchema(validationSchema, false))
-
-        if (!naoGerarTodasRotas) {
-            this.gerarRotaBusca()
-            this.gerarRotaDeleta()
-            this.gerarRotaAtualiza()
-            this.gerarRotaAdicionaUm()
-        }
-    }
-
-    gerarValidationSchema(validationSchema, withoutNullChecker) {
-        const copyWithoutNullChecker = JSON.parse(JSON.stringify(validationSchema))
-        const keys = Object.keys(copyWithoutNullChecker)
-        for (let i = 0; i < keys.length; i++) {
-            const k = keys[i]
-
-            copyWithoutNullChecker[k].in = ["query", "body"]
-
-            if (withoutNullChecker || copyWithoutNullChecker[k].optional) {
-                copyWithoutNullChecker[k].optional = {
+            },
+            "*": {
+                optional: {
                     options: {
                         nullable: true
                     }
                 }
             }
-        }
-        return copyWithoutNullChecker
+        })
+
+        this.attrs = keys
+        this.attrsQuery = Object.keys(copy)
+
+        return copy
     }
 
     gerarRotaBusca() {
-        this.router.get(`/${this.nomePlural}`, checkSchema(this.validationSchemaWithoutNullChecker), async (req, res) => {
+        this.router.get(`/${this.nomePlural}`, checkSchema(this.validationSchema), async (req, res) => {
             try {
                 await this.inicio(req, res, `buscando ${this.nomePlural}...`)
 
-                const resultado = await this.DAO.get(req.query)
+                const query = await this.gerarQuery(req, res)
+
+                const resultado = await this.DAO.get(query)
                 res.status(200).json(resultado)
 
                 this.fim(req, res)
@@ -115,11 +109,13 @@ module.exports = class Controller {
     }
 
     gerarRotaDeleta() {
-        this.router.delete(`/${this.nomePlural}`, checkSchema(this.validationSchemaWithoutNullChecker), async (req, res) => {
+        this.router.delete(`/${this.nomePlural}`, checkSchema(this.validationSchema), async (req, res) => {
             try {
                 await this.inicio(req, res, `deletando ${this.nomePlural}...`)
 
-                const resultado = await this.DAO.delete(req.query)
+                const query = await this.gerarQuery(req, res)
+
+                const resultado = await this.DAO.delete(query)
                 res.status(202).json(resultado)
 
                 this.fim(req, res)
@@ -130,11 +126,14 @@ module.exports = class Controller {
     }
 
     gerarRotaAtualiza() {
-        this.router.post(`/${this.nomePlural}`, checkSchema(this.validationSchemaWithoutNullChecker), async (req, res) => {
+        this.router.post(`/${this.nomePlural}`, checkSchema(this.validationSchema), async (req, res) => {
             try {
                 await this.inicio(req, res, `atualizando ${this.nomePlural}...`)
 
-                const resultado = await this.DAO.update(req.body, req.query)
+                const body = await this.gerarBodyUpdate(req, res)
+                const query = await this.gerarQuery(req, res)
+
+                const resultado = await this.DAO.update(body, query)
                 res.status(202).json(resultado)
 
                 this.fim(req, res)
@@ -149,7 +148,9 @@ module.exports = class Controller {
             try {
                 await this.inicio(req, res, `adicionando ${this.nomeSingular}...`)
 
-                const resultado = await this.DAO.add(req.body)
+                const body = await this.gerarBodyAdd(req, res)
+
+                const resultado = await this.DAO.add(body)
                 res.status(201).json(resultado)
 
                 this.fim(req, res)
@@ -159,12 +160,63 @@ module.exports = class Controller {
         })
     }
 
+    async gerarJSON(req, res, location, attrs, obligatory, allObligatory) {
+        let o = {}
+
+        const errors = []
+
+        for (let i = 0; i < attrs.length; i++) {
+            const attr = attrs[i]
+            const value = req[location][attr]
+
+            if(obligatory && obligatory.includes(attr)){
+                if(value === undefined && allObligatory){
+                    errors.push(await this.formatError(attr, value, "O valor não pode ser nulo.", location))
+                }
+                else if(value === null){
+                    errors.push(await this.formatError(attr, value, "O valor não pode ser nulo.", location))
+                }
+                else {
+                    o[attr] = value
+                }
+            }
+            else if(value){
+                o[attr] = value
+            }
+        }
+
+        if (Object.keys(o).length === 0) {
+            res.status(400).json(await this.formatError(undefined, undefined, `O request ${location} está vazio ou não possue algum atributo válido.`))
+
+            throw new Error("Empty object.")
+        }
+
+        if (errors.length > 0) {
+            res.status(400).json(errors)
+
+            throw new Error("Not Null error.")
+        }
+
+        return o
+    }
+
+    async gerarBodyAdd(req, res) {
+        return this.gerarJSON(req, res, "body", this.attrs, this.attrNotNull, true)
+    }
+
+    async gerarBodyUpdate(req, res) {
+        return this.gerarJSON(req, res, "body", this.attrs, this.attrNotNull, false)
+    }
+
+    async gerarQuery(req, res) {
+        return this.gerarJSON(req, res, "query", this.attrsQuery)
+    }
+
     async errorHandler(erro, req, res) {
-        if (erro.message.includes("Validation Errors.")) {
-            res.status(400).json(validationResult(req))
-        } else {
-            console.log(erro)
-            res.status(500).json(await this.formatError(undefined, undefined, "Erro no servidor"))
+        console.log(erro)
+        const ok = erro.message.includes("Validation Errors.") || erro.message.includes("Empty object.") || erro.message.includes("Not Null error.")
+        if (!ok) {
+            res.status(500).json(await this.formatError(undefined, undefined, "Erro no servidor."))
         }
         this.fim(req, res)
     }
@@ -183,7 +235,11 @@ module.exports = class Controller {
 
     async inicio(req, res, mensagem) {
         console.log(`request id: ${req.id} -> ${mensagem}`)
-        if (validationResult(req).errors.length > 0) {
+
+        const validationErrors = validationResult(req)
+        if (validationErrors.errors.length > 0) {
+            res.status(400).json(validationErrors)
+
             throw new Error("Validation Errors.")
         }
     }
