74cec4cb68dbcbaf08bf15750e1ca32a1740df24
igorXVI <inazumaseleven04@gmail.com>
2019-08-29 18:33:08 -0300

    arrumando bugs na party relationships controller e kpi surveys controller

--- src/app/controllers/KpiSurveyController.js
+++ src/app/controllers/KpiSurveyController.js
@@ -29,43 +29,16 @@ module.exports = class KpiSurveyController extends Controller {
 
     
     async gerarJSON(req, res, location, attrs, obligatory, allObligatory) {
-        let o = {}
-
-        let errors = []
+        let o = await super.gerarJSON(req, res, location, attrs, obligatory, allObligatory)
 
         const re = (await this.DAO.get({
-            survey_id: { $eq: req.body.surveyId },
-            kpi_id: { $eq: req.body.kpiId }
+            survey_id: { $eq: o.survey_id },
+            kpi_id: { $eq: o.kpi_id }
         }))[0]
 
         if(re !== undefined){
-            res.status(400).json(await this.formatError("[surveyId, kpiId]", [req.body.surveyId, req.body.kpiId], "Não é possivel cadastrar diferentes kpis-surveys para mesma kpi"))
+            res.status(400).json(await this.formatError("[surveyId, kpiId]", [o.survey_id, o.kpi_id], "A combinação de foreign keys já está cadastrada.", "body"))
             throw new Error("Validation Errors.")
-        }
-
-        for (let i = 0; i < attrs.length; i++) {
-            const attr = attrs[i]
-            const value = req[location][attr]
-
-            if (value === undefined && allObligatory) {
-                errors.push(await this.formatError(attr, value, "O valor deve ser informado.", location))
-            } else if (value === null && obligatory && obligatory.includes(attr)) {
-                errors.push(await this.formatError(attr, value, "O valor não pode ser nulo.", location))
-            } else if (value !== undefined) {
-                o[_.snakeCase(attr)] = value
-            }
-        }
-
-        if (Object.keys(o).length === 0) {
-            res.status(400).json(await this.formatError(undefined, undefined, `O request ${location} está vazio ou não possue algum atributo válido.`))
-
-            throw new Error("Empty object.")
-        }
-
-        if (errors.length > 0) {
-            res.status(400).json(errors)
-
-            throw new Error("Not Null error.")
         }
 
         return o
