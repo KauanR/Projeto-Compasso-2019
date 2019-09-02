81374d8570be668c92ea001e1699e069009819bc
Danilo <danilo.marmentini@gmail.com>
2019-09-02 16:01:36 -0300

    criação de metodo para gravar dados na tabela survey_items

--- src/app/controllers/SurveyItemsController.js
+++ src/app/controllers/SurveyItemsController.js
@@ -1,4 +1,5 @@
 const Controller = require("./Controller")
+const CriteriaController = require("./CriteriaController")
 
 module.exports = class SurveyItemsController extends Controller {
     constructor() {
@@ -9,32 +10,62 @@ module.exports = class SurveyItemsController extends Controller {
                 errorMessage: "O date deve ser uma data no formato ISO8601 (AAAA-MM-DD)."
             },
             partyId: {
-                isInt: {
-                    options: {
-                        min: 1
-                    }
-                },
                 notNull: true,
-                errorMessage: "O party_id deve ser inteiro maior que 0."
+                fk: "parties/party"
             },
             criteriaId: {
-                isInt: {
-                    options: {
-                        min: 1
-                    }
-                },
                 notNull: true,
-                errorMessage: "O criteria_id deve ser inteiro maior que 0."
+                fk: "criterias/criteria"
             },
             criteriaKpiId: {
-                isInt: {
-                    options: {
-                        min: 1
-                    }
-                },
                 notNull: true,
-                errorMessage: "O criteria_kpi_id deve ser inteiro maior que 0."
+                fk: "kpis/kpi"
             }
         })
+
+        this.criteriaController = new CriteriaController()
     }
+
+    async adicionaUm(req, res) {
+        try {
+            await this.inicio(req, res, `adicionando ${this.nomeSingular}...`)
+
+            let resultado = undefined
+
+            req.body.date = new Date()
+
+            if (req.body.criterias !== undefined && req.body.criterias instanceof Array) {
+                resultado = []
+
+                const criterias = req.body.criterias
+                delete req.body.criterias
+
+                for (let i = 0; i < criterias.length; i++) {
+                    const criteria = (await this.criteriaController.gerarBusca({
+                        query: {
+                            id: {
+                                $eq: criterias[i].id
+                            },
+                            except: "description,type,value"
+                        }
+                    }, res))[0]
+
+                    req.body.criteriaId = criteria.id
+                    req.body.criteriaKpiId = criteria.kpi.id
+
+                    resultado.push(await this.gerarAdicao(req, res))
+                }
+            }
+            else {
+                resultado = await this.gerarAdicao(req, res)
+            }
+
+            res.status(201).json(resultado)
+
+            this.fim(req, res)
+        } catch (error) {
+            this.errorHandler(error, req, res)
+        }
+    }
+
 }
